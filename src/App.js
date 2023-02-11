00961387f19628de510dee24220d8e900f3780d1
JPedroVentura <joao-ventura55@hotmail.com>
2023-02-11 15:18:37 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { FaFileDownload } from 'react-icons/fa'
+import React from 'react';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [qrcodeLink, setQrcodeLink] = useState('');
+
+  const handleGenerate = (link) => {
+    QRCodeLink.toDataURL(link, {
+      width: 600,
+      margin: 3
+    }, (err, url) => {
+      setQrcodeLink(url);
+    })
+  }
+
+  const handleQRcode = (e) => {
+    setUrl(e.target.value);
+    handleGenerate(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>:: QRCODE GEN ::</h1>
+      <QRCode value={url}></QRCode>
+      <input className="input form-control" placeholder='https://www.youtube.com/watch?v=' onChange={event => handleQRcode(event)}></input>
+      {url === '' ? <span className='span'>Aguardando link...</span> : <a href={qrcodeLink} download={'qrcode.png'}> <FaFileDownload></FaFileDownload> </a>}
+
     </div>
   );
 }
